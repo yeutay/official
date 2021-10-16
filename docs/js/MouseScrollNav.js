4ce5c828ae76409bbe1419568d0a2ed85262a186
bcoffee <blackcat980630@gmail.com>
2021-10-16 21:57:06 +0800

    Optimized website
    add mask production and change all image to .webp format

--- docs/js/MouseScrollNav.js
+++ docs/js/MouseScrollNav.js
@@ -1,12 +1,14 @@
 var prevScrollpos = window.pageYOffset;
 
 window.onscroll = function(){
-    var currentScrollpos = window.pageYOffset;
-    if (prevScrollpos > currentScrollpos){
-        document.getElementById("navbar").style.top = "0";
+    if (window.pageYOffset > 150){
+        var currentScrollpos = window.pageYOffset;
+        if (prevScrollpos > currentScrollpos){
+            document.getElementById("navbar").style.top = "0";
+        }
+        else{
+            document.getElementById("navbar").style.top = "-300px";
+        }
+        prevScrollpos = currentScrollpos;
     }
-    else{
-        document.getElementById("navbar").style.top = "-300px";
-    }
-    prevScrollpos = currentScrollpos;
 }
